14fd1c2cb28b856663ff765dc5a37ed6da488772
TheLuckyman30 <bnauta0@gmail.com>
2024-04-09 12:03:43 -0400

    Added Home Page button into header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
-export function Header () {
+import { Button } from "react-bootstrap";
+import { SwitchPages } from "../interfaces/SwitchPages";
+
+export function Header ({ setCurrentPage }: SwitchPages) {
+
+    function changeToHome () {
+        setCurrentPage(0);
+    }
+
     return (
-        <div></div>
+        <Button onClick={changeToHome}>Home Page</Button>
     )
 }
