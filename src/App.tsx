90925afff3497e7e092bc5c09b6918367659b614
TheLuckyman30 <bnauta0@gmail.com>
2024-04-10 00:22:51 -0400

    Fixed state variables and functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import { BasicQuestions } from './BasicQuestions';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -26,23 +27,7 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Brandon Nauta</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React NERDS !!! I am a nerd, Taylor Swift 
-        </a>
-        <p> Nathan Rowell </p>
-        <p> Selin Bacaz </p>
-      </header>
+      <BasicQuestions></BasicQuestions>
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -4,12 +4,7 @@ import { ShowProgressBar } from "./components/ProgressBar";
 const answerArray: string[] = ["", "", "", "", "", "", "", "", "",""]
 
 export function BasicQuestions(): JSX.Element {
-    const [userAnswer, setUserAnswer] = useState<string>("");
-    let numQuestionsAnswered: number = 0;
-
-    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
-        setUserAnswer(event.target.value);
-    }
+    const [numQuestionsAnswered, setNumQuestionsAnswered] = useState<number>(0);
 
     function Description(): JSX.Element {
         return (
@@ -20,121 +15,181 @@ export function BasicQuestions(): JSX.Element {
     }
 
     function Question1(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[0] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 1: What is most important to you in a job between: Salary, Work Life Balance, Growth, Helping Others, Making a Difference</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[0] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question2(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[1] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 2: Do you prefer working solo or with others?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[1] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question3(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[2] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 3: What is your favorite subject in school?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[2] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
     
     function Question4(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[3] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 4: What type of workplace suits you best between: Fast-Paced, Collaborative, Structured, Flexible?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[3] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question5(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[4] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 5: On a scale of 1-10, how much do you like working with technology?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[4] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question6(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[5] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 6: On a scale of 1-10, how much do you enjoy doing art?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[5] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question7(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[6] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 7: Do you prefer working indoors or outdoors?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[6] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question8(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[7] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 8: Do you enjoy coming up with new ideas and solutions?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[7] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question9(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[8] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 9: Do you like tasks that vary each day, or stay the same?</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[8] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
 
     function Question10(): JSX.Element {
+        const [userAnswer, setUserAnswer] = useState<string>("");
+
+        function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+            setUserAnswer(event.target.value);
+            answerArray[9] = userAnswer;
+            setNumQuestionsAnswered(numQuestionsAnswered + 1);
+        }
+
         return (
             <div>
                 <h3>Question 10: How much education do you plan to pursue? (High School Diploma, Bachelors, Masters, Doctorate)</h3>
                 <input type="text" value={userAnswer} onChange={updateAnswer} />
-                <div>userAnswer</div>
-                {answerArray[9] = userAnswer};
-                {numQuestionsAnswered++};
+                <div>{userAnswer}</div>
             </div>
         );
     }
