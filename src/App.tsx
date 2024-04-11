5f57e905c28d95d0cbb9a2a7031a026173cf9f63
TheLuckyman30 <bnauta0@gmail.com>
2024-04-10 20:11:18 -0400

    Revrted App.tsx to its original state

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,23 @@ function App() {
   }
   return (
     <div className="App">
-      <BasicQuestions></BasicQuestions>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Brandon Nauta</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React NERDS !!! I am a nerd, Taylor Swift 
+        </a>
+        <p> Nathan Rowell </p>
+        <p> Selin Bacaz </p>
+      </header>
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
