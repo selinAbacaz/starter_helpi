9367a16adeb232d1a7c8221a024649cdc7385b2e
TheLuckyman30 <bnauta0@gmail.com>
2024-04-17 01:11:06 -0400

    Navbar almost finished

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,25 @@
+import { Nav, Navbar } from "react-bootstrap";
 import { SwitchPages } from "../interfaces/SwitchPages";
 import { SwitchPage } from "./SwitchPage";
 
 export function NavBar ({setCurrentPage, pageNumber}: SwitchPages) {
     const buttonNames: string[] = ["Home", "Basic Questions", "Detailed Questions"];
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <span className="navbar-brand">{buttonNames[pageNumber]}</span>
-            <ul className="navbar-nav">
-                <li>
-                    <span className="nav-link"><SwitchPage setCurrentPage={setCurrentPage} pageNumber={0} varaint={"outline-primary"}></SwitchPage></span>
-                </li>
-                <li>
-                    <span className="nav-link"><SwitchPage setCurrentPage={setCurrentPage} pageNumber={1} varaint={"outline-primary"}></SwitchPage></span>
-                </li>
-                <li>
-                    <span className="nav-link"><SwitchPage setCurrentPage={setCurrentPage} pageNumber={2} varaint={"outline-primary"}></SwitchPage></span>
-                </li>
-            </ul>
-        </nav>
+        <div>
+        <Navbar sticky="top">
+            <Navbar.Brand style={{fontSize: "25px"}}>Career Helpi</Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                    <Nav.Link><SwitchPage setCurrentPage={setCurrentPage} pageNumber={0} varaint={"me-auto"}></SwitchPage></Nav.Link>
+                    <Nav.Link><SwitchPage setCurrentPage={setCurrentPage} pageNumber={1} varaint={"me-auto"}></SwitchPage></Nav.Link>
+                    <Nav.Link><SwitchPage setCurrentPage={setCurrentPage} pageNumber={2} varaint={"me-auto"}></SwitchPage></Nav.Link>
+                </Nav>
+                <Navbar.Collapse className="justify-content-center">
+                    <Navbar.Text style={{fontSize: "25px"}}>{buttonNames[pageNumber]}</Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar.Collapse>
+        </Navbar>
+        </div>
+        
     )
 }
