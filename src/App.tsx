e965f1a299eaa4a0310991ecf727f9d6285f2e1e
nathanrowell <nrowell@udel.edu>
2024-04-18 11:58:51 -0400

    Added Result Page to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Button, Form } from 'react-bootstrap';
 import { HomePage } from './homePage';
 import { BasicQuestions } from './BasicQuestions';
 import { DetailedQuestions } from './DetailedQuestions';
+import { ResultsPage } from './resultsPage';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -33,6 +34,7 @@ function App() {
         {currentPage === 0 && <HomePage setCurrentPage={setCurrentPage} pageNumber={0}></HomePage>}
         {currentPage === 1 && <BasicQuestions setCurrentPage={setCurrentPage} pageNumber={1}></BasicQuestions>}
         {currentPage === 2 && <DetailedQuestions setCurrentPage={setCurrentPage} pageNumber={2}></DetailedQuestions>}
+        {currentPage === 3 && <ResultsPage setCurrentPage={setCurrentPage} pageNumber={3}></ResultsPage>}
         <Form>
           <Form.Label>API Key:</Form.Label>
           <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
--- src/resultsPage.tsx
+++ src/resultsPage.tsx
@@ -1,1 +1,53 @@
+import './homePage.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import { SwitchPages } from './interfaces/SwitchPages';
+
+export function ResultsPage ({setCurrentPage}: SwitchPages) {
+
+    return(
+        <div>
+            <title> header of page </title>
+            <div>
+                <header >
+                    <div style={ {border: '3px', fontSize: 16, padding: '8px', color: "#ff6347", backgroundColor: "white"} }>
+                        <div className="right"> <p>Home | Account | LogOut | Results</p> </div>
+                        <p> Result Home Page</p> 
+                    </div>
+
+                </header>
+                <body style={ {border: '3px white', padding: '4px', color: "#44506a", backgroundColor: "#aebcda", justifyContent:"right"} }>
+                    <div >
+                    <Container >
+                    <Row >
+                        <Col className=" box2">
+                                Overview
+                        </Col>
+                        <Col>
+                            <Row style={{display:"flex"}}>
+                            <Col className= "  red box">
+                                Industry 
+                            </Col>
+                            <Col className= "  green box">
+                                Specific Jobs
+                            </Col>
+                            </Row>
+                            <Row style={{display:"flex"}}>
+                                <Col className= "  pink box">
+                                Other
+                                </Col>
+                                <Col className= "  orange box">
+                                Other 2
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    </Container>
+                    </div>
+                </body>
+            </div>
+        </div>
+    );
+}
+
+
 export {}
