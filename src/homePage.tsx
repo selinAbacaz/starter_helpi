3bee53e6d872159d4eddbd23bceee2930a44af79
selin <selinb@udel.edu>
2024-04-20 03:12:37 -0400

    changed color of home page

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -13,10 +13,12 @@ export function HomePage ({setCurrentPage}: SwitchPages) {
                     <div style={ {border: '3px', fontSize: 16, padding: '8px', color: "#ff6347", backgroundColor: "white"} }>
                         <div className="right"> <p>Home | Account | LogOut | Results</p> </div>
                         <p> Quiz Home Page</p> 
+                        <hr style={{height: 5, backgroundColor: "salmon", marginBottom:30, color: "salmon"}}></hr>
                     </div>
-
                 </header>
-                <body style={ {border: '3px white', padding: '4px', color: "#44506a", backgroundColor: "#aebcda", justifyContent:"right"} }>
+                
+                <title> this is where the 'boxes' of the page are- aka the body with the buttons </title>
+                <body style={ {border: '3px white', padding: '4px', color: "#44506a", backgroundColor: "white", justifyContent:"right"} }>
                     <div >
                     <Container >
                     <Row >
@@ -25,15 +27,15 @@ export function HomePage ({setCurrentPage}: SwitchPages) {
                         </Col>
                         <Col>
                             <Row style={{display:"flex", justifyContent:"right"}}>
-                            <Col className= "  red box">
+                            <Col className= "box" style= {{backgroundColor: "salmon"}}>
                                 <SwitchPage setCurrentPage={setCurrentPage} pageNumber={1} /*Basic Question Button*/></SwitchPage> 
                             </Col>
-                            <Col className= "  green box">
+                            <Col className= "box" style= {{backgroundColor: "#ff6347"}}>
                                 Take the basic career assessment to discover a career path personally picked for you! The assessment works with advanced artificial intelligence to analyze your personality traits, interests, and values to provide personalized recommendations specifically for the user. The basic quiz consists of 10 shorter questions to provide a quick and easy experience to determine your results.
                             </Col>
                             </Row>
                             <Row style={{display:"flex", justifyContent:"right"}}>
-                                <Col className= "  pink box">
+                                <Col className= "box" style= {{backgroundColor: "#FE8A2E"}}>
                                     <SwitchPage setCurrentPage={setCurrentPage} pageNumber={2} /*Detailed Question Button*/></SwitchPage>
                                 </Col>
                                 <Col className= "  orange box">
