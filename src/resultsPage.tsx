53fac4dc93bbec067d4a0647befacf844162f961
nathanrowell <nrowell@udel.edu>
2024-04-23 11:11:35 -0400

    updates to resultpage

--- src/resultsPage.tsx
+++ src/resultsPage.tsx
@@ -15,61 +15,25 @@ export function ResultsPage ({setCurrentPage}: SwitchPages) {
                     </div>
 
                 </header>
-                <body style={ {border: '3px white', padding: '4px', color: "#44506a", backgroundColor: "#aebcda", justifyContent:"right"} }>
+                <body style={ {border: '3px white', padding: '4px', color: "#44506a", backgroundColor: "#F4E9E2", justifyContent:"right"} }>
+
                     <div >
-                    <Container >
-                        <Row>
-                        <Col className = "navBar" style={{width: "3px"}}> 
-                        Overview 
-                        Industry
-                        Specific Jobs 
-                        Other 
-                    </Col>
-                    <Col>
-                    <Row>
-                            <Col className ="box2">
-                                Overview
-                            </Col>
-                            <Col className = "textBox" style = {{width: "200px"}}>
-                                TEXT WILL BE ENTERED HERE
-                            </Col>
-                            </Row>
-                        <Row >
-                            <Col className = "red box" style={{display:"flex"}}>
-                                Industry 
-                                </Col>
-                                <Col className = "textBox">
-                                TEXT WILL BE ENTERED HERE
-                            </Col>
-                        </Row>
-                        <Row >
-                            <Col className= "  green box">
-                            Specific Jobs
-                            </Col>
-                            <Col className = "textBox">
-                                TEXT WILL BE ENTERED HERE
-                            </Col>
-                        </Row>
-                        <Row >
-                            <Col className = " pink box" style={{display:"flex"}}>
-                                Other
-                                </Col>
-                                <Col className = "textBox">
-                                TEXT WILL BE ENTERED HERE.
-                            </Col>
-                        </Row>
-                        <Row >
-                            <Col className = " orange box">
-                                Other 2
-                                </Col>
-                                <Col className = "textBox">
-                                TEXT WILL BE ENTERED HERE
-                            </Col>
-                        </Row>
-                    </Col>
-                        </Row>
-                   
-                    </Container>
+                    <Col className = "textBox">
+                            
+                            
+                    <p> Congratulations! You've just finished a complete assessment of your interests and personality and you're well on your way to discovering your ideal career path. </p>
+
+<p>In this free basic report, you'll see a summary of your scores in each of the six career interest areas. You'll learn more about what these scores mean, and how your top interest area can show which careers you are suited for. </p>
+
+<p>Then, we'll show you how key personality traits can point you toward a career that takes advantage of your natural strengths.</p>
+
+<p>Finally, we'll show you how to unlock your full report to get an in-depth profile of your interests and personality, along with personalized career planning advice and a complete listing of careers that match your individual interest profile.</p>
+
+<p>So, let's get started!</p>
+<h1> Your Work Style </h1>
+</Col>
+
+                    
                     </div>
                 </body>
             </div>
