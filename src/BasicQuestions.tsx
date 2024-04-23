4f060d990ecf3b4254d51ed1163550edb843de44
TheLuckyman30 <bnauta0@gmail.com>
2024-04-23 17:37:31 -0400

    Added multiple input types

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ShowProgressBar } from "./components/ProgressBar";
 import { ShowAlert } from "./components/Alert";
 import { BlurPageProps } from "./interfaces/BlurPage";
-import { Button, Col, Row } from 'react-bootstrap';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import "./App.css"
 import './BasicQuestions.css';
 
@@ -27,6 +27,19 @@ const questionsArray: string[] =
     "Question 9: Do you like tasks that vary each day, or stay the same?",
     "Question 10: How much education do you plan to pursue? (High School Diploma, Bachelors, Masters, Doctorate)"
 ];
+const optionsArrays: string[][] = 
+[
+    ["Salary", "Work Life Balance", "Growth", "Helping others", "Making a Difference"],
+    ["Solo", "With Others"],
+    [""],
+    ["Fast-Paced", "Collaborative", "Structured", "Flexible"],
+    [""],
+    [""],
+    ["Indoors", "Outdoors"],
+    ["Yes", "No"],
+    ["I like tasks that vary", "I like tasks that stay the same"],
+    [""]
+]
 
 function Question ({setNumQuestionAnswered, question, answerPlacement, submitted}: BasicQuestionsProps) {
     const [userAnswer, setUserAnswer] = useState<string>(answerArray[answerPlacement]);
@@ -35,6 +48,43 @@ function Question ({setNumQuestionAnswered, question, answerPlacement, submitted
         setUserAnswer(event.target.value);
         answerArray[answerPlacement] = event.target.value;
         setNumQuestionAnswered(answerArray.reduce((totalAnswered: number, answer: string) => answer !== "" ? totalAnswered + 1 : totalAnswered, 0));
+    }
+
+    if (answerPlacement !== 2 && answerPlacement !== 4 && answerPlacement !== 5 && answerPlacement !== 9) {
+        return (
+            <div>
+                <h3 style={{marginBottom:20}}>{question}</h3>
+                {optionsArrays[answerPlacement].map((option: string) => (
+                    <Form.Check
+                        key={option}
+                        inline
+                        id={option}
+                        type="radio"
+                        name={answerPlacement.toString()}
+                        label={option}
+                        value={option}
+                        onChange={updateAnswer}
+                        disabled={submitted}>
+                    </Form.Check>
+                ))}
+                <div>{userAnswer}</div>
+                <hr style={{height: 5, backgroundColor: "white", marginBottom:60, color: "white"}}></hr>
+            </div>
+        );
+    }
+    else if (answerPlacement === 4 || answerPlacement === 5) {
+        return (
+            <div>
+                <h3 style={{marginBottom:20}}>{question}</h3>
+                <Form.Range
+                    min={1}
+                    max={10}>
+                </Form.Range>
+                <hr style={{height: 5, backgroundColor: "white", marginBottom:60, color: "white"}}></hr>
+            </div>
+        );
+        
+        
     }
     
     return (
