4508faa1a7f204f76e4b738dbd416a8f5d030506
selin <selinb@udel.edu>
2024-04-23 12:16:52 -0400

    added a bunch of formatting on detail questions also fixed a bunch of stuff on switchpage

--- src/resultsPage.tsx
+++ src/resultsPage.tsx
@@ -22,15 +22,15 @@ export function ResultsPage () {
                             
                     <p> Congratulations! You've just finished a complete assessment of your interests and personality and you're well on your way to discovering your ideal career path. </p>
 
-<p>In this free basic report, you'll see a summary of your scores in each of the six career interest areas. You'll learn more about what these scores mean, and how your top interest area can show which careers you are suited for. </p>
+                    <p>In this free basic report, you'll see a summary of your scores in each of the six career interest areas. You'll learn more about what these scores mean, and how your top interest area can show which careers you are suited for. </p>
 
-<p>Then, we'll show you how key personality traits can point you toward a career that takes advantage of your natural strengths.</p>
+                    <p>Then, we'll show you how key personality traits can point you toward a career that takes advantage of your natural strengths.</p>
 
-<p>Finally, we'll show you how to unlock your full report to get an in-depth profile of your interests and personality, along with personalized career planning advice and a complete listing of careers that match your individual interest profile.</p>
+                    <p>Finally, we'll show you how to unlock your full report to get an in-depth profile of your interests and personality, along with personalized career planning advice and a complete listing of careers that match your individual interest profile.</p>
 
-<p>So, let's get started!</p>
-<h1> Your Work Style </h1>
-</Col>
+                    <p>So, let's get started!</p>
+                    <h1> Your Work Style </h1>
+                    </Col>
 
                     
                     </div>
