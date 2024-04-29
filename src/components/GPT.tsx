0d3ef34425e90e9bd2ffde2babe3701d1a684634
TheLuckyman30 <bnauta0@gmail.com>
2024-04-29 19:30:47 -0400

    Changing around GPT code to prepare for prompt engineering

--- src/components/GPT.tsx
+++ src/components/GPT.tsx
@@ -3,9 +3,10 @@ import { detailedAnswerArray, detailedQuestionsArray } from "../DetailedQuestion
 import { keyData } from "../App";
 import OpenAI from "openai";
 
-async function formatQuestionsAndAnswers (userPrompt: string, page: string) {
-  let combinedQuestions: string = "";
-  let combinedAnswers: string = "";
+let combinedQuestions: string = "";
+let combinedAnswers: string = "";
+
+function formatQuestionsAndAnswers (page: string) {
   if (page === "basic") {
     combinedQuestions = basicQuestionsArray.join(",");
     combinedAnswers = (basicAnswerArray.map((answer: string): string => 
@@ -20,28 +21,64 @@ async function formatQuestionsAndAnswers (userPrompt: string, page: string) {
     `${detailedAnswerArray.indexOf(answer) + 1}` + 
     " Answer: " + answer)).join(",");
   }
-
-  const result: string = await callGPT(combinedQuestions, combinedAnswers, userPrompt)
-  return result;
 }
 
-async function callGPT (combinedQuestions: string, combinedAnswers: string, userPrompt: string) {
+async function callGPT (type: string, userPrompt: string) {
+  let result: OpenAI.Chat.Completions.ChatCompletion;
   const openai = new OpenAI(
     {
       apiKey: keyData,
       dangerouslyAllowBrowser: true
-    });
-  const result = await openai.chat.completions.create({
-    messages: 
-    [
-      { role: "user", content: userPrompt},
-      { role: "system", content: "Use these questions as context: " + combinedQuestions + ". Use these answeres as context: " + combinedAnswers + ". Do not mention the orginal questions and answers in your response. If the user asks for specific industries, give a list of specific indsutries that would suit them"}
-    ],
-      model: "gpt-4-turbo"
-  });
+    }
+  );
+
+  if (type === "indsutry") {
+    result = await openai.chat.completions.create(
+      {
+        messages: 
+        [
+          { role: "user", content: "Give a list of specific industries that would fit me. Please give me a few reasons as to why."},
+          { role: "system", content: "Use these questions as context: " + combinedQuestions + ". Use these answeres as context: " + combinedAnswers + 
+          ". Output your respones in JSON."}
+        ],
+          model: "gpt-4-turbo",
+          response_format: { type: "json_object" } 
+      }
+    );
+  }
+  
+  else if (type === "overview") {
+    result = await openai.chat.completions.create(
+      {
+        messages: 
+        [
+          { role: "user", content: "Please provide an overview of what my results mean."},
+          { role: "system", content: "Use these questions as context: " + combinedQuestions + ". Use these answeres as context: " + combinedAnswers + 
+          ". Do not mention the orginal questions and answers in your response. If the user asks for specific industries, give a list of specific indsutries that would suit them. Output your respones in JSON."}
+        ],
+          model: "gpt-4-turbo",
+      }
+    );
+  }
+
+  else {
+    result = await openai.chat.completions.create(
+      {
+        messages: 
+        [
+          { role: "user", content: userPrompt},
+          { role: "system", content: "Use these questions as context: " + combinedQuestions + ". Use these answeres as context: " + combinedAnswers + 
+          ". Do not mention the orginal questions and answers in your response. If the user asks for specific industries, give a list of specific indsutries that would suit them. Output your respones in JSON."}
+        ],
+          model: "gpt-4-turbo",
+      }
+    );
+  }
 
   if (result.choices[0].message.content) {
+      console.log(result);
     return (
+      
       result.choices[0].message.content
     );
   }
@@ -51,17 +88,8 @@ async function callGPT (combinedQuestions: string, combinedAnswers: string, user
 }
 
 export async function GenerateText (type: string, page: string, userInput: string) {
-  let result = "";
-  if (type === "industry") {
-    result = await formatQuestionsAndAnswers("Give a list of specific industries that would fit me, add a few bullet points as to why.", page);
-  }
-  else if (type === "detailed") {
-    result = await formatQuestionsAndAnswers("Please provide an overview of what my results mean.", page);
-  }
-  else {
-    result = await formatQuestionsAndAnswers(userInput, page);
-  }
+  formatQuestionsAndAnswers(page);
   return (
-    result
+    await callGPT(type, userInput)
   );
 }
