9fcc95a30e54d72e7c1185258f7c51144c195878
TheLuckyman30 <bnauta0@gmail.com>
2024-05-07 03:00:56 -0400

    Pie chart now displays on detailed questions results

--- src/pages/results/ResultsPage.tsx
+++ src/pages/results/ResultsPage.tsx
@@ -1,7 +1,7 @@
 import './ResultsPage.css';
 import { Col, Form} from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-import { GenerateText, saveGPTReplyBaic, saveGPTReplyDetailed, saveIndustriesBasic, saveIndustriesDetailed, saveOverviewBasic, saveOverviewDetailed } from './GPT';
+import { GenerateText, saveGPTReplyBaic, saveGPTReplyDetailed, saveIndustriesBasic, saveIndustriesDetailed, saveOverviewBasic, saveOverviewDetailed, savePieChartValuesBasic, savePieChartValuesDetailed } from './GPT';
 import ResultsSection from './components/ResultsSection';
 import { ResultsPageProps } from '../../interfaces/ResultsPage';
 
@@ -12,19 +12,21 @@ export function ResultsPage ({ setQuestionsToUse, setSubmitFlagBasic, setSubmitF
     const [industriesDetailed, setIndustriesDetailed] = useState<string>(saveIndustriesDetailed); // Contains the list of industries chatGPT returns for the detailed questions
     const [chatGPTReplyBasic, setChatGPTReplyBasic] = useState<string>(saveGPTReplyBaic); // Contains chatGPTs reply to the users input for basic questions
     const [chatGPTReplyDetailed, setChatGPTReplyDetailed] = useState<string>(saveGPTReplyDetailed); // Contains chatGPTs reply to the users input for detailed questions
-    const [pieChartValues, setPieChartValues] = useState<string>("");
+    const [pieChartValuesBasic, setPieChartValuesBasic] = useState<string>(savePieChartValuesBasic);
+    const [pieChartValuesDetailed, setPieChartValuesDetailed] = useState<string>(savePieChartValuesDetailed);
 
     useEffect(() => {
         if (submitFlagBasic) {
             GenerateText("overview", "basic", "", setOverviewBasic);
             GenerateText("industry", "basic", "", setIndustriesBasic);
-            GenerateText("pie", "basic", "", setPieChartValues);
+            GenerateText("pie", "basic", "", setPieChartValuesBasic);
             console.log("basic");
             setSubmitFlagBasic(false);
         }
         if (submitFlagDetailed) {
             GenerateText("overview", "detailed", "", setOverviewDetailed);
             GenerateText("industry", "detailed", "", setIndustriesDetailed);
+            GenerateText("pie", "detailed", "", setPieChartValuesDetailed);
             console.log("detailed");
             setSubmitFlagDetailed(false);
         }
@@ -44,8 +46,8 @@ export function ResultsPage ({ setQuestionsToUse, setSubmitFlagBasic, setSubmitF
                                 <option value="basic">Basic Questions</option>
                                 <option value="detailed">Detailed Questions</option>
                             </Form.Select>
-                            {questionsToUse === "basic" && industriesBasic && overviewBasic && <ResultsSection setGPTReply={setChatGPTReplyBasic} chatGPTReply={chatGPTReplyBasic} industries={industriesBasic} overview={overviewBasic} pieChartValues={pieChartValues} questionsToUse={questionsToUse}></ResultsSection>}
-                            {questionsToUse === "detailed" && industriesDetailed && overviewDetailed && <ResultsSection setGPTReply={setChatGPTReplyDetailed} chatGPTReply={chatGPTReplyDetailed} industries={industriesDetailed} overview={overviewDetailed} pieChartValues={pieChartValues} questionsToUse={questionsToUse}></ResultsSection>}
+                            {questionsToUse === "basic" && industriesBasic && overviewBasic && <ResultsSection setGPTReply={setChatGPTReplyBasic} chatGPTReply={chatGPTReplyBasic} industries={industriesBasic} overview={overviewBasic} pieChartValues={pieChartValuesBasic} questionsToUse={questionsToUse}></ResultsSection>}
+                            {questionsToUse === "detailed" && industriesDetailed && overviewDetailed && <ResultsSection setGPTReply={setChatGPTReplyDetailed} chatGPTReply={chatGPTReplyDetailed} industries={industriesDetailed} overview={overviewDetailed} pieChartValues={pieChartValuesDetailed} questionsToUse={questionsToUse}></ResultsSection>}
                         </Col>
                     </div>
                 </div>
