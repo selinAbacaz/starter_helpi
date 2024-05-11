0b78d2f8d94bbac6331688c3b1e568b1a826b231
selin <selinb@udel.edu>
2024-05-10 23:39:29 -0400

    did some resizing of homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -11,19 +11,20 @@ import { HomePageProps } from '../../interfaces/HomePage';
 export function HomePage ({setBlurPage, setCurrentPage, blurPage}: HomePageProps) {
 
     return(
-        <div>
-            <div className= "backgrColor" style={ {border: '3px white', padding: '4px', justifyContent:"right"} }>
+        <div className= "backgrColor">
+            <div>
                 <div>
                     <Container style= {{minWidth: "95%"}}>
                         
-                        <Row  style={{minHeight: "120%"}}>
+                        <Row  style={{gridRow: "100%"}}>
                             {/* this row contains top part of homepage with the image, buttons, and short descriptions */}
-                            <Col className="HPImageBox" style={{marginRight: "1%", minHeight: "120%"}}>
+                            <Col>
+                                <Row className="HPImageBox" style={{marginRight: ".01%", height: "111%"}}></Row>
                             </Col>
 
                             <Col>
-                                <Row style={{display:"flex", justifyContent:"right"}}>
-                                    <Col className= "buttonBox1 LouisGeorgeNormal" style={{marginBottom: "1%", marginRight: "1%"}}>
+                                <Row style={{display:"flex", justifyContent:"right",marginBottom: "1%", height: "55%"}}>
+                                    <Col className= "buttonBox1 LouisGeorgeNormal" style={{ marginRight: "1%"}}>
                                         <SwitchPage setCurrentPage={setCurrentPage} currentPage={1} variant={"primary"} type={"button"} blurPage={blurPage} setBlurPage={setBlurPage}/*Basic Question Button*/></SwitchPage>
                                     </Col>
                                     <Col className= "box magilio" style= {{backgroundColor: "#ff6347",color:"#f9e0d1", opacity: "75%", marginBottom: "1%"}}>
@@ -33,7 +34,7 @@ export function HomePage ({setBlurPage, setCurrentPage, blurPage}: HomePageProps
                                          
                                     </Col>
                                 </Row>
-                                <Row style={{display:"flex", justifyContent:"right"}}>
+                                <Row style={{display:"flex", justifyContent:"right",marginBottom: "1%", height: "55%"}}>
                                     <Col className= "buttonBox2 LouisGeorgeNormal" style={{marginRight: "1%"}}>
                                         <SwitchPage setCurrentPage={setCurrentPage} currentPage={2} variant={"primary"} type={"button"} blurPage={blurPage} setBlurPage={setBlurPage}/*Detailed Question Button*/></SwitchPage>
                                     </Col>
@@ -47,6 +48,9 @@ export function HomePage ({setBlurPage, setCurrentPage, blurPage}: HomePageProps
                             
                         </Row>
 
+                    </Container>
+
+                    <Container style= {{marginTop: "5%", minWidth: "95%"}}>
                         <Row style= {{marginTop:"2%", backgroundColor: "#855440"}}>
                                 <h1 className="magilio" style={{color:"white", fontSize: "400%", opacity: "80%", marginTop: "5%", marginBottom: "2%", letterSpacing:5}}> Results made reliable with AI</h1>
                                 <hr style={{height: 4, width: "60%", backgroundColor: "white", marginLeft: "20%", borderRadius:80, color: "white", marginBottom: "5%"}}></hr>
@@ -84,7 +88,8 @@ export function HomePage ({setBlurPage, setCurrentPage, blurPage}: HomePageProps
                         </Row>
 
                     </Container>
-                
+
+                    
                 </div>
                 <br></br>
                 
