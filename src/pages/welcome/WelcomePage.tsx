8d45b5b0965e9953b2320af1c9fec6db3bc9bc40
TheLuckyman30 <bnauta0@gmail.com>
2024-05-15 03:15:57 -0400

    Moved text into a two column setup

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -1,9 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+
 function WelcomePage () {
     return (
-        <div style={{fontSize: "75px"}}>
-            Welcome to the WEBSITE_NAME
-            <br></br>
-            (Powered by chatGPT)
+        <div style={{fontSize: "75px", minHeight: "100vh"}}>
+            <Row style={{height: "100vh"}}>
+                <Col style={{justifyContent: "left", alignItems: "center", display: "flex"}}>
+                    Welcome to the 
+                    <br></br>
+                    WEBSITE_NAME
+                    <br></br>
+                    (Powerd by Ai)
+                </Col>
+                <Col>
+                    
+                </Col>
+            </Row>
         </div>
     )
 }
