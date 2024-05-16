43b9b5fb9581caf73c861d1c7e0d853797ea0284
TheLuckyman30 <bnauta0@gmail.com>
2024-05-16 02:16:01 -0400

    Changed style of api submit button on welcome page

--- src/components/ApiKeyInput.tsx
+++ src/components/ApiKeyInput.tsx
@@ -57,7 +57,7 @@ function ApiKeyInput ({ setSubmittedNewKey, setValidKey, blurPage, type }: ApiKe
                         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey} disabled={blurPage} onKeyDown={handleEnterSubmit}></Form.Control>
                     </Col>
                     <Col>
-                        <Button className="Submit-Button" onClick={handleSubmit} disabled={blurPage}>Submit</Button>
+                        <Button className="Submit-Button" variant={"outline-secondary"} onClick={handleSubmit} disabled={blurPage}>Submit</Button>
                     </Col>
                 </Row>
                 
