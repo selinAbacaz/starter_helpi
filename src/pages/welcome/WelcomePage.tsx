5f73777f6ef0b6a15fcd36e9f736eeba9b359bd6
TheLuckyman30 <bnauta0@gmail.com>
2024-05-16 00:20:51 -0400

    Added white semi-transparent box around welcome page text

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -8,25 +8,30 @@ import '../../assets/LouisGeorgeCafe/stylesheet.css'
 function WelcomePage ({ setCurrentPage, setSubmittedNewKey, setValidKey, validKey }: WelcomePageProps) {
     return (
         <div className="full-height welcome">
-            <Row className="full-height d-flex margins animations LouisGeorgeNormal">
-                <Col className="column d-flex">
-                    <div>
-                        <div className="title-text">
-                            Welcome to the 
+            <Row className="full-height d-flex row-margins animations LouisGeorgeNormal">
+                <Col className="text-column d-flex">
+                    <div className="text-background">
+                        <div className="text-margins">
+                            <div className="title-text">
+                                Welcome to the 
+                                <br></br>
+                                WEBSITE_NAME
+                            </div>
+                            <div className="ai-text">
+                                (Powered by AI)  
+                            </div>
                             <br></br>
-                            WEBSITE_NAME
+                            <div className="api-text">
+                                <p>The WEBSITE_NAME uses chatGPT to provide accurate results.</p>
+                                {!validKey && <p>Please provide an OpenAI API key before moving forward.</p>}
+                                {validKey && <p>You're all set! Enjoy the quiz!</p>}
+                            </div>
+                            <div>
+                                {!validKey && <ApiKeyInput setSubmittedNewKey={setSubmittedNewKey} setValidKey={setValidKey} blurPage={false} type={"welcome"}></ApiKeyInput>}
+                                {validKey && <SwitchPage setCurrentPage={setCurrentPage} currentPage={0} type={"welcome"}></SwitchPage>}
+                            </div>
                         </div>
-                        <div className="ai-text">
-                            (Powered by AI)  
-                        </div>
-                        <br></br>
-                        <div className="api-text">
-                            <p>The WEBSITE_NAME uses chatGPT to provide accurate results.</p>
-                            {!validKey && <p>Please provide an OpenAI API key before moving forward.</p>}
-                            {validKey && <p>You're all set! Enjoy the quiz!</p>}
-                        </div>
-                        {!validKey && <ApiKeyInput setSubmittedNewKey={setSubmittedNewKey} setValidKey={setValidKey} blurPage={false} type={"welcome"}></ApiKeyInput>}
-                        {validKey && <SwitchPage setCurrentPage={setCurrentPage} currentPage={0} type={"welcome"}></SwitchPage>}
+                        
                     </div>      
                 </Col>
                 <Col>
